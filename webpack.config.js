6901b4eaf756700cfb610897abc2d077952eee6c
Jordan Grubb <imjordangrubb@gmail.com>
2020-11-17 11:13:40 -0500

    Changes before package delete

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE.ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build/',
+    // proxy: {
+    //   '/api': 'http://localhost:3000',
+    // },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
